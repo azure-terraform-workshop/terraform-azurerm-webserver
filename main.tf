914260d4e2be2e7a95ce278b297b4002a35af6cf
Gabe Howe <ghowe@rms.com>
2019-08-21 10:49:53 -0500

    0.12upgrade

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "azurerm_resource_group" "module" {
   name     = "${local.module_name}-rg"
-  location = "${var.location}"
+  location = var.location
 
-  tags {
+  tags = {
     environment = "dev"
     version     = "v0.0.2"
   }
@@ -10,56 +10,57 @@ resource "azurerm_resource_group" "module" {
 
 resource "azurerm_network_interface" "module" {
   name                = "${local.module_name}-nic${count.index}"
-  location            = "${azurerm_resource_group.module.location}"
-  resource_group_name = "${azurerm_resource_group.module.name}"
-  count               = "${var.count}"
+  location            = azurerm_resource_group.module.location
+  resource_group_name = azurerm_resource_group.module.name
+  count               = var.vm_count
 
   ip_configuration {
     name                          = "ipconfig-${count.index}"
-    subnet_id                     = "${var.subnet_id}"
+    subnet_id                     = var.subnet_id
     private_ip_address_allocation = "dynamic"
   }
 
-  tags {
+  tags = {
     environment = "dev"
   }
 }
 
 resource "azurerm_virtual_machine" "module" {
   name                             = "${local.module_name}-vm${count.index}"
-  location                         = "${azurerm_resource_group.module.location}"
-  resource_group_name              = "${azurerm_resource_group.module.name}"
-  network_interface_ids            = ["${element(azurerm_network_interface.module.*.id, count.index)}"]
-  count                            = "${var.count}"
-  vm_size                          = "${var.size}"
-  delete_os_disk_on_termination    = "${var.delete_os_disk_on_termination}"
+  location                         = azurerm_resource_group.module.location
+  resource_group_name              = azurerm_resource_group.module.name
+  network_interface_ids            = [element(azurerm_network_interface.module.*.id, count.index)]
+  count                            = var.vm_count
+  vm_size                          = var.size
+  delete_os_disk_on_termination    = var.delete_os_disk_on_termination
   delete_data_disks_on_termination = true
 
   storage_image_reference {
-    publisher = "${element(split(":",var.os), 0)}"
-    offer     = "${element(split(":",var.os), 1)}"
-    sku       = "${element(split(":",var.os), 2)}"
-    version   = "${element(split(":",var.os), 3)}"
+    publisher = element(split(":", var.os), 0)
+    offer     = element(split(":", var.os), 1)
+    sku       = element(split(":", var.os), 2)
+    version   = element(split(":", var.os), 3)
   }
 
   storage_os_disk {
     caching           = "ReadWrite"
     create_option     = "FromImage"
     name              = "${local.module_name}-vm${count.index}-os"
-    managed_disk_type = "${var.disk_os_sku}"
+    managed_disk_type = var.disk_os_sku
   }
 
   os_profile {
     computer_name  = "${local.module_name}vm${count.index}"
-    admin_username = "${var.username}"
-    admin_password = "${var.password}"
+    admin_username = var.username
+    admin_password = var.password
   }
 
   os_profile_linux_config {
     disable_password_authentication = false
   }
 
-  tags {
+  tags = {
     environment = "dev"
   }
 }
+
--- variables.tf
+++ variables.tf
@@ -22,32 +22,33 @@ locals {
   module_name = "${var.name}-webserver"
 }
 
-variable "count" {
+variable "vm_count" {
   default     = 1
-  type        = "string"
+  type        = string
   description = "The number of VMs to provision"
 }
 
 variable "os" {
   default     = "OpenLogic:CentOS:7.3:latest"
-  type        = "string"
+  type        = string
   description = "The Marketplace image information in the following format: Offer:Publisher:Sku:Version"
 }
 
 variable "size" {
   default     = "Standard_D1_v2"
-  type        = "string"
+  type        = string
   description = "VM SKU to provision"
 }
 
 variable "disk_os_sku" {
   default     = "Standard_LRS"
-  type        = "string"
+  type        = string
   description = "Managed disk SKU for the OS disk"
 }
 
 variable "delete_os_disk_on_termination" {
   default     = false
-  type        = "string"
+  type        = string
   description = "Flag to delete OS disk on VM destroy"
 }
+
